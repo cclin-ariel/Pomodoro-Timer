39f6e0345b98bc00e1120626989d4a0d0fc106ae
cclin <71698978+cclin-ariel@users.noreply.github.com>
2021-06-13 08:49:22 +0900

    step donw function

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ var workSessionDuration = 1500; //25 mins
 var currentTimeLeftInSession = 1500; //25 mins
 var breakSessionDuration = 300; //5 mins
 
+var type = "Work";
+var timeSpentIncurrentSession = 0;
+
 //show the current time
 var currentTime = setInterval(() => {
     var d = new Date();
@@ -36,6 +39,7 @@ var currentTime = setInterval(() => {
 }, 1000);
 
 //toggleClock function
+//Toggle between work and break sessions 
 
 const toggleClock = reset => {
     if (reset) {
@@ -52,7 +56,30 @@ const toggleClock = reset => {
 
             // decrease time left & increase time spent  
             clockTimer = setInterval(() => {
-                currentTimeLeftInSession--;
+                stepDown();
+
+                //step donw function
+                const stepDown = () => {
+                    if (currentTimeLeftInSession > 0) {
+                        currentTimeLeftInSession--;
+                        timeSpentIncurrentSession++;
+                    } else if (currentTimeLeftInSession === 0) {
+                        timeSpentIncurrentSession = 0;
+
+                        //display a log of our completed sessions
+                        if (type == "Work") {
+                            currentTimeLeftInSession = breakSessionDuration;
+                            displySessionLog("Work");
+                            type = "Break";
+                        } else {
+                            currentTimeLeftInSession = workSessionDuration;
+                            displaySessionLog("Break");
+                            type = "Work";
+                        }
+
+                    }
+                }
+
                 displayCurrentTimeLeftInSession(); //show the left time
             }, 1000);
         }
@@ -90,9 +117,10 @@ const stopClock = () => {
     displayCurrentTimeLeftInSession();
 }
 
-//Toggle between work and break sessions 
-//display a log of our completed sessions
+//displaySessionLog function
+const displaySessionLog =
 
-//Customize the work label and the duration of the sessions
 
-//Add a circular progress bar to the timer
+    //Customize the work label and the duration of the sessions
+
+    //Add a circular progress bar to the timer
